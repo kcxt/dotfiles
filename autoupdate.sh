da86a2d9dd395fa6a4f57a127009d8f98bebd34d
Caleb Connolly <caleb@connolly.tech>
2020-08-16 21:52:59 +0100

    Auto-update: 2 files changed.

---
+++ autoupdate.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+WORKDIR=/home/caleb/dotfiles
+pushd $WORKDIR
+
+LOGFILE=$WORKDIR/dotfiles-update.log
+ORIGIN=$(git remote get-url origin | sed "s/https:\/\///g")
+
+source $WORKDIR/.GITHUB_KEY
+# Now GITHUB_KEY is the auth token
+
+CHANGES=$(git status --porcelain=v1 2>/dev/null | wc -l)
+
+function hasChanged(){
+    echo "Changes since last run, pushing..."
+    git add .
+    COMMIT_MSG="Auto-update: $CHANGES files changed."
+    git commit -m "$COMMIT_MSG"
+    echo https://calebccff:$GITHUB_KEY@$ORIGIN
+    git push -u https://calebccff:$GITHUB_KEY@$ORIGIN master 2>&1 >> $LOGFILE
+}
+
+[[ $CHANGES -gt 0 ]] && hasChanged
+
+popd
